e1866469ef3fb35180b7ca7002d569c1eb7260cd
YUU Yabu <yuuyabu220@gmail.com>
2025-08-24 21:17:16 +0900

    TypeScript用設定を追加
    - biomeでany型使用できるように設定を変更

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    include: ["src/**/*.test.js"],
+    include: ["src/**/*.test.js", "src/**/*.test.ts"],
     coverage: {
-      include: ["src/**/*.js"],
-      exclude: ["src/**/*.test.js"],
+      include: ["src/**/*.js", "src/**/*.ts"],
+      exclude: ["src/**/*.test.js", "src/**/*.test.ts"],
       reporter: ["text", "html", "lcov"],
     },
     globals: true,
