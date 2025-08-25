125edf6971e9bc3d3b47ba10429b1087b8445bc1
okou <29059223+ookkoouu@users.noreply.github.com>
2025-08-25 11:04:04 +0000

    Typescript構成をコミット前型検査用に調整 - pre-commitに型検査を実行 - Nightly拡張機能は不要なので削除 - デフォルトなビルドフラグを削除

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    include: ["src/**/*.test.js", "src/**/*.test.ts"],
+    include: ["src/**/*.test.{js,ts}"],
     coverage: {
-      include: ["src/**/*.js", "src/**/*.ts"],
-      exclude: ["src/**/*.test.js", "src/**/*.test.ts"],
+      include: ["src/**/*.{js,ts}"],
+      exclude: ["src/**/*.test.{js,ts}"],
       reporter: ["text", "html", "lcov"],
     },
     globals: true,
